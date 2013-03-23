162591b375cbaef9d0034f5bb9b36bd20bcd027b
Ian Lunn <hello@ianlunn.co.uk>
2013-03-23 16:38:00 +0000

    Added 2 new themes

---
+++ themes/basic-slide/sequencejs-options.basic-slide.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    var options = {
+        navigationSkip: false,
+        transitionThreshold: false
+    };
+
+    var sequence = $("#sequence").sequence(options).data("sequence");
+});
