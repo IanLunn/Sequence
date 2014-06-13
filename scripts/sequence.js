e78462569671324a5e635508b1fabddadc4dfbd8
Ian Lunn <hello@ianlunn.co.uk>
2014-06-13 10:21:19 +0100

    Updated bower.json and changed Sequence's version to semver

--- scripts/sequence.js
+++ scripts/sequence.js
@@ -4,7 +4,7 @@
  *
  * @link https://github.com/IanLunn/Sequence
  * @author IanLunn
- * @version 2.0.0 Alpha Alpha
+ * @version 2.0.0-alpha
  * @license https://github.com/IanLunn/Sequence/blob/master/LICENSE
  * @copyright IanLunn
  */
